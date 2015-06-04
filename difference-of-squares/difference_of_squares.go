eed85511cfebb89db9d8dc44f0b4531ce0c5b6c5
jgu <jeffwgu@gmail.com>
2015-06-04 07:27:26 -0600

    indent normally

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -8,7 +8,6 @@ func SumOfSquares(n int) int {
 	sum := 0
 	for i := 1; i < n+1; i++ {
 		sum = sum + i*i
-
 	}
 	return sum
 }
--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -1,25 +1,25 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	var a = [][]int{{1}}
-	if n > 1 {
-		for i := 0; i < n-1; i++ {
-			a = append(a, CreateNextRow(a[i]))
-		}
-	}
-	return a
+  var a = [][]int{{1}}
+  if n > 1 {
+    for i := 0; i < n-1; i++ {
+      a = append(a, CreateNextRow(a[i]))
+    }
+  }
+  return a
 }
 
 func CreateNextRow(prev_row []int) []int {
-	var next_row []int
-	if len(prev_row) == 1 {
-		next_row = append(next_row, 1, 1)
-	} else {
-		next_row = append(next_row, 1)
-		for i := 0; i < len(prev_row)-1; i++ {
-			next_row = append(next_row, prev_row[i] + prev_row[i+1])
-		}
-		next_row = append(next_row, 1)
-	}
-	return next_row
+  var next_row []int
+  if len(prev_row) == 1 {
+    next_row = append(next_row, 1, 1)
+  } else {
+    next_row = append(next_row, 1)
+    for i := 0; i < len(prev_row)-1; i++ {
+      next_row = append(next_row, prev_row[i] + prev_row[i+1])
+    }
+    next_row = append(next_row, 1)
+  }
+  return next_row
 }
